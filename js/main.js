651ce75fc248805d764e6101a21bb747dea15f7f
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-29 18:35:18 +0000

    Implemented transform functionality for car image based on keydown event key. Also fixed an error on my previous commit where I placed the event listener on my car variable instead of the window object. Changed car into an object with an image property and direction property.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,113 @@
-var $car = document.querySelector('#car-img');
-$car.addEventListener('keydown', handleInput);
+var car = {
+  $image: document.querySelector('#car-img'),
+  direction: 'right'
+};
+
+window.addEventListener('keydown', handleInput);
 
 function handleInput(event) {
-
+  switch (event.key) {
+    case 'ArrowUp':
+      car.direction = 'up';
+      turnCar();
+      break;
+    case 'ArrowDown':
+      car.direction = 'down';
+      turnCar();
+      break;
+    case 'ArrowLeft':
+      car.direction = 'left';
+      turnCar();
+      break;
+    case 'ArrowRight':
+      car.direction = 'right';
+      turnCar();
+      break;
+    default:
+      break;
+  }
 }
+
+function turnCar(direction) {
+  switch (car.direction) {
+    case 'left':
+      car.$image.style.transform = 'rotate(-180deg)';
+      break;
+    case 'right':
+      car.$image.style.transform = 'rotate(0)';
+      break;
+    case 'down':
+      car.$image.style.transform = 'rotate(90deg)';
+      break;
+    case 'up':
+      car.$image.style.transform = 'rotate(-90deg)';
+  }
+}
+
+// function turnUp() {
+//   switch (car.direction) {
+//     case 'left':
+//       car.$image.style.transform = 'rotate(90deg)';
+//       break;
+//     case 'right':
+//       car.$image.style.transform = 'rotate(-90deg)';
+//       break;
+//     case 'down':
+//       car.$image.style.transform = 'rotate(180deg)';
+//       break;
+//     case 'up':
+//       car.$image.style.transform = 'rotate(0)';
+//   }
+//   car.direction = 'up';
+// }
+
+// function turnDown() {
+//   switch (car.direction) {
+//     case 'left':
+//       car.$image.style.transform = 'rotate(-90deg)';
+//       break;
+//     case 'right':
+//       car.$image.style.transform = 'rotate(90deg)';
+//       break;
+//     case 'down':
+//       car.$image.style.transform = 'rotate(0deg)';
+//       break;
+//     case 'up':
+//       car.$image.style.transform = 'rotate(180deg)';
+//   }
+//   car.direction = 'down';
+// }
+
+// function turnLeft() {
+//   switch (car.direction) {
+//     case 'left':
+//       car.$image.style.transform = 'rotate(0deg)';
+//       break;
+//     case 'right':
+//       car.$image.style.transform = 'rotate(180deg)';
+//       break;
+//     case 'down':
+//       car.$image.style.transform = 'rotate(90deg)';
+//       break;
+//     case 'up':
+//       car.$image.style.transform = 'rotate(-90deg)';
+//   }
+//   car.direction = 'left';
+// }
+
+// function turnRight() {
+//   switch (car.direction) {
+//     case 'left':
+//       car.$image.style.transform = 'rotate(180deg)';
+//       break;
+//     case 'right':
+//       car.$image.style.transform = 'rotate(0eg)';
+//       break;
+//     case 'down':
+//       car.$image.style.transform = 'rotate(0)';
+//       break;
+//     case 'up':
+//       car.$image.style.transform = 'rotate(90deg)';
+//   }
+//   car.direction = 'right';
+// }
